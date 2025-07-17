89f1cc2b9038324cc1fb8d447a52bdb3e326d8e1
Itsumi Mario <lumarioortiz@gmail.com>
2025-07-16 18:52:43 -0600

    ajustes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react()],
+  output: "server",
+  adapter: vercel({}),
   vite: {
     plugins: [tailwindcss()],
   },
